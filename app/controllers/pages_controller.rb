486ec110f5e36d62785ab14461d0176ea02f2e4a
CyrilDebon <cyril.debon@gmail.com>
2019-02-12 13:50:26 +0100

    gem modified, controller tasks index and views made, model too

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def task
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get pages_index_url
+    assert_response :success
+  end
+
+  test "should get task" do
+    get pages_task_url
+    assert_response :success
+  end
+
+end
